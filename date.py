4d854b67ab67f2c47b089df6dc24deeabe939d8d
Peng Ziqiao <xfyx.student@sina.com>
2017-08-09 16:37:11 +0800

    Update date.py

--- date.py
+++ date.py
@@ -2,6 +2,10 @@ import datetime
 import calendar
 
 class Week():
+    """
+    monday, sunday 为上周一、日
+    N 为上周周数
+    """
     sunday = datetime.date.today()
     while sunday.weekday() != calendar.SUNDAY:
         sunday -= datetime.timedelta(days=1)
@@ -15,3 +19,16 @@ class Week():
         elif day == 'end':
             day = self.sunday
         return f'{day.strftime(str_format)}'
+
+    def history(self, i):
+        """
+        从上周起往前的第i周
+        :return: (monday, sunday, N)
+        """
+        monday = self.monday - datetime.timedelta(weeks=i)
+        sunday = self.sunday - datetime.timedelta(weeks=i)
+        N = self.N - i
+        return (monday,sunday,N)   
+
+class Month():
+    N = datetime.date.today().month
