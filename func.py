02661e57ed8b01373016b40d7bab3cd0f1c58a4c
Peng Ziqiao <xfyx.student@sina.com>
2017-08-09 16:36:40 +0800

    Update func.py

--- func.py
+++ func.py
@@ -9,7 +9,8 @@ def gr(a,b, desc=True, degree=2):
         return round(r, degree) 
 
 def cagr(current_value, start_value , years, desc=True, degree=2):
-    """复合增长率
+    """
+    复合增长率
     """
     r = (current_value / start_value) ** (1 / years) - 1
     if desc:
@@ -18,8 +19,29 @@ def cagr(current_value, start_value , years, desc=True, degree=2):
         return round(r, degree) 
 
 def percent(value, degree=2):
-    change = '下降' if value < 0 else '增长'
+    """
+    将带正负号的比值(1代表100%)转成“增长/下降xx%”的形式
+    """
+    from numpy import isnan
+    if isnan(value):
+        return '无环比'
+    else:
+        change = '下降' if value < 0 else '增长'
     value = abs(value) * 100
-    value = round(value, degree)
+    if degree == 0:
+        value = f'{value:.0f}'
+    else:
+        value = round(value, degree)
     return f'{change}{value}%'
 
+def px2cm(value):
+    """
+    将像素换算成厘米
+    """
+    return value/96*2.54
+
+def cm2px(value):
+    """
+    将厘米换算成像素
+    """
+    return value/2.54*96
